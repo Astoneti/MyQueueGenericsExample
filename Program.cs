d72f2c1d406ff1275437b5e98f9bfd655cecbd38
Anton Pashkun <a.pashkun@godeltech.com>
2021-02-25 16:47:17 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyQueueGenericsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем новую очередь.
+            Queue<string> myQueue = new Queue<string>();
+
+            // Добавляем новые элементы в очередь.
+            myQueue.Enqueue("One");
+            myQueue.Enqueue("Two");
+            myQueue.Enqueue("Three");
+            Console.WriteLine($"\nThe queue contains : {myQueue.Count} elements.");
+
+            // Получаем элементы с удалением.
+            string item1 = myQueue.Dequeue();
+            Console.WriteLine($"\nFirst element from queue : {item1}.");
+            string item2 = myQueue.Dequeue();
+            Console.WriteLine($"\nSecond element from queue : {item2}.");
+
+            // Добавляем новый элемент в очередь.
+            myQueue.Enqueue("\nNew element - Ten");
+          
+            // Просматриваем элемент без удаления.
+            string item3 = myQueue.Peek();
+            Console.WriteLine($"\nView the element without remove : {item3}.");
+            myQueue.Clear();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Queue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MyQueueGenericsExample
+{
+    class Queue <T>
+    {
+        private int size;        // текущий размер стека
+        private List<T> data;
+        public int Count { get { return data.Count; } }
+        public Queue ()
+        {
+            data = new List<T>();
+            size++;
+        }
+
+        private T GetItem()
+        {
+          //  Получаем первый элемент.
+           T item = data.FirstOrDefault();
+
+           // Если элемент пуст, то сообщаем об ошибке.
+            if (item == null)
+            {
+                throw new Exception("Queue is empty,no elements for use.");
+            }
+            return item;
+        }
+
+        public void Enqueue(T item)   //Добавить элемент в очередь.
+        {
+            data.Add(item);
+            size++;
+        }
+
+        //Получить элемент из очереди с удалением.
+        public T Dequeue ()
+        {
+            // Получить элемент из начала очереди.
+            T item = data.FirstOrDefault();
+            // Удаляем элемент из коллекции.
+            data.Remove(item);
+            size--;
+            // Возвращаем полученный элемент.
+            return item;
+        }
+
+        // чтение главного элемента очереди без удаления
+        public T Peek()
+        {
+            // Получаем элемент из начала очереди.
+            T item = GetItem();
+            // Возвращаем полученный элемент.
+            return item;
+        }
+
+        // Очищаем список
+        public void Clear()
+        {
+            data.Clear();
+        }
+    }
+}
